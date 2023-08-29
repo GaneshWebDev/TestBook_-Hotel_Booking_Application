2fba244a6b6f3e7e7cfa219bdf46850fb2bc41ea
Ganesh_web_dev <115787522+GaneshWebDev@users.noreply.github.com>
2023-08-29 11:02:06 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 const express=require('express');
 const dbConfig=require('./db');
+const cors = require('cors');
 const bodyParser=require('body-parser');
 const roomsRoute=require('./routes/roomsRoute');
 const userRoute=require('./routes/userRoute');
 const bookroomRoute=require('./routes/bookingRoute')
 const app=express();
+app.use(cors({
+  origin: 'http://localhost:3000', // Adjust this to match your frontend's URL
+}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(roomsRoute);
